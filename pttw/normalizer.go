f744bd6ce121af81c0689950f5523a26b3f5473d
ordon <ordon1313@gmail.com>
2015-11-29 20:39:21 +0100

    normalizer for slice without absolute value

--- pttw/normalizer.go
+++ pttw/normalizer.go
@@ -1,5 +1,20 @@
 package pttw
 
 func Normalize(slice []float64) []float64 {
-	return []float64{1}
+	max := Max(slice)
+	normalized := []float64{}
+	for i := 0; i < len(slice); i++ {
+		normalized = append(normalized, slice[i] / max)
+	}
+	return normalized
+}
+
+func Max(slice []float64) float64 {
+	max := slice[0]
+	for i := 0; i < len(slice); i++ {
+		if (max < slice[i]) {
+			max = slice[i]
+		}
+	}
+	return max
 }
--- pttw/normalizer_test.go
+++ pttw/normalizer_test.go
@@ -10,3 +10,33 @@ func TestNormalizerShouldNormalizeSingleElement(t *testing.T) {
 	normalized := Normalize(slice)
 	assert.That(normalized[0]).IsEqualTo(float64(1))
 }
+
+func TestNormalizerShouldNormalizeSameElements(t *testing.T) {
+	assert := assert.New(t)
+	slice := []float64{3, 3}
+	normalized := Normalize(slice)
+	assert.That(normalized[0]).IsEqualTo(float64(1))
+	assert.That(normalized[1]).IsEqualTo(float64(1))
+}
+
+func TestMaxShouldFindMax(t *testing.T) {
+	assert := assert.New(t)
+	slice := []float64{6}
+	max := Max(slice)
+	assert.That(max).IsEqualTo(float64(6))
+}
+
+func TestMaxShouldFindMaxFromMultipleElements(t *testing.T) {
+	assert := assert.New(t)
+	slice := []float64{3, 7, 2}
+	max := Max(slice)
+	assert.That(max).IsEqualTo(float64(7))
+}
+
+func TestNormalizerShouldNormalizeDifferentElements(t *testing.T) {
+	assert := assert.New(t)
+	slice := []float64{3,6}
+	normalized := Normalize(slice)
+	assert.That(normalized[0]).IsEqualTo(float64(0.5))
+	assert.That(normalized[1]).IsEqualTo(float64(1))
+}
