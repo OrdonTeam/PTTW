59b3b4747f7c216462e27ad86a3b6580bc0b9ca2
ordon <ordon1313@gmail.com>
2015-11-30 00:49:22 +0100

    input params with dashes

--- main/main.go
+++ main/main.go
@@ -18,7 +18,11 @@ func main() {
 }
 
 func inputFile() *os.File {
-	file, err := os.Open(os.Args[1])
+	name := findParam("-in", os.Args)
+	if (name == "") {
+		return os.Stdin
+	}
+	file, err := os.Open(name)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -26,7 +30,11 @@ func inputFile() *os.File {
 }
 
 func outputFile() *os.File {
-	file, err := os.Create(os.Args[2])
+	name := findParam("-out", os.Args)
+	if (name == "") {
+		return os.Stdout
+	}
+	file, err := os.Create(name)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -34,9 +42,22 @@ func outputFile() *os.File {
 }
 
 func snr() float64 {
-	snr, err := strconv.ParseFloat(os.Args[3], 64)
+	value := findParam("-snr", os.Args)
+	if (value == "") {
+		return 20
+	}
+	snr, err := strconv.ParseFloat(value, 64)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	return snr
+}
+
+func findParam(param string, args []string) string {
+	for i := 0; i < len(args) - 1; i++ {
+		if (args[i] == param) {
+			return args[i + 1]
+		}
+	}
+	return ""
 }
