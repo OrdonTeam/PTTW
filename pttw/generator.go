0b436d9f37417d81520799c5d64dec2fa9205558
ordon <ordon1313@gmail.com>
2015-11-29 21:45:42 +0100

    generator is using provided function

--- pttw/generator.go
+++ pttw/generator.go
@@ -1,1 +1,11 @@
 package pttw
+
+func Generator(nextRandom func() float64) (func(int) []float64) {
+	return func(size int) []float64 {
+		slice := []float64{}
+		for i := 0; i < size; i++ {
+			slice = append(slice, nextRandom())
+		}
+		return slice
+	}
+}
--- pttw/generator_test.go
+++ pttw/generator_test.go
@@ -2,9 +2,26 @@ package pttw
 import (
 	"github.com/assertgo/assert"
 	"testing"
+	"math/rand"
 )
 
-func TestGeneratorShouldGenerate(t *testing.T) {
+func TestGeneratorShouldGenerateRandomSliceInGivenSize(t *testing.T) {
 	assert := assert.New(t)
-	assert.ThatBool(true).IsTrue()
+	slice := Generator(rand.Float64)(5)
+	assert.That(len(slice)).IsEqualTo(5)
+}
+
+func TestGeneratorShouldUseRandomFunction(t *testing.T) {
+	assert := assert.New(t)
+	slice := Generator(arrayBasedRandomProvider([]float64{1, 2}))(2)
+	assert.That(slice[0]).IsEqualTo(float64(1))
+	assert.That(slice[1]).IsEqualTo(float64(2))
+}
+
+func arrayBasedRandomProvider(array []float64) func() float64 {
+	index := 0
+	return func() float64 {
+		index++
+		return array[index-1]
+	}
 }
