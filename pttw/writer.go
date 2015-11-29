ed498417b7533d6287703d874a8ff2ad5b7001da
ordon <ordon1313@gmail.com>
2015-11-29 22:44:19 +0100

    writer

--- pttw/writer.go
+++ pttw/writer.go
@@ -6,12 +6,21 @@ import (
 )
 
 func Write(slice []float64, writer io.Writer) error {
-	for i := 0; i < len(slice); i++ {
-		bytes := []byte(strconv.FormatFloat(slice[i], 'f', -1, 64))
-		_, err := writer.Write(bytes)
+	for i := 0; i < len(slice) - 1; i++ {
+		err := writeFloat(slice[i], writer)
+		if err != nil {
+			return err
+		}
+		_, err = writer.Write([]byte(" "))
 		if err != nil {
 			return err
 		}
 	}
-	return nil
+	return writeFloat(slice[len(slice) - 1], writer)
+}
+
+func writeFloat(value float64, writer io.Writer) error {
+	bytes := []byte(strconv.FormatFloat(value, 'f', -1, 64))
+	_, err := writer.Write(bytes)
+	return err
 }
--- pttw/writer_test.go
+++ pttw/writer_test.go
@@ -13,3 +13,11 @@ func TestWriterShouldWriteSlice(t *testing.T) {
 	assert.That(true).IsEqualTo(true)
 	assert.That(buffer.String()).IsEqualTo("1")
 }
+
+func TestWriterShouldWriteSliceDelimitedWithSpaces(t *testing.T) {
+	assert := assert.New(t)
+	var buffer bytes.Buffer
+	Write([]float64{1, 2}, &buffer)
+	assert.That(true).IsEqualTo(true)
+	assert.That(buffer.String()).IsEqualTo("1 2")
+}
