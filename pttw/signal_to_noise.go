897b35875c6a0bd4dd183f36df352540e0a50239
ordon <ordon1313@gmail.com>
2015-11-29 23:32:51 +0100

    add signal to noise should use SNR

--- pttw/signal_to_noise.go
+++ pttw/signal_to_noise.go
@@ -2,8 +2,9 @@ package pttw
 
 func AddSignalToNoise(signal []float64, noise []float64, snr float64) []float64 {
 	sum := []float64{}
+	multiplier := SnrToLinear(snr)
 	for i := 0; i < len(signal); i++ {
-		sum = append(sum, signal[i] + noise[i])
+		sum = append(sum, signal[i] + noise[i] / multiplier)
 	}
 	return sum
 }
